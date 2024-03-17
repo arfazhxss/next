e9176ba68b1d0de2c23d0886707010ac75fa54ad
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-03-17 12:47:07 -0700

    initial updates Commit by @arfazhxss on Sun 17 Mar 2024

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button size="default">
+        Click this little thing
+      </Button>
+    </div>
+  );
+}
